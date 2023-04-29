9b65ee1625bdce50e0e209bb09c6f27efeb9ea2c
jonathanphill <jonathanphill@gmail.com>
2023-04-29 19:48:37 -0400

    finally figured the red line under the feature links when clicked

--- src/script.js
+++ src/script.js
@@ -1,17 +1,27 @@
 console.log('it works!');
 const featureAnchors = document.querySelectorAll('.featureAnchor');
 const sections = document.querySelectorAll('section');
+const spans = document.getElementsByTagName('span');
+console.log(spans);
 
 featureAnchors.forEach((anchor)=>{
     anchor.addEventListener('click', (event)=>{
-      const href = event.target.hash;
-      sections.forEach((section) => {
+        // console.log(event.currentTarget);
+        for(let span in spans){
+            spans[span].className="";
+        }
+        const href = event.currentTarget.hash;
+        sections.forEach((section) => {
         if('#'+section.id === href){
+            // console.log('true: ' + href);
+            event.target.classList.toggle("redbottomStroke");
             section.classList.remove('hidden');
         }else{
+            // console.log('false: '+ href);
             section.classList.add('hidden');
         }
       });
+
 
     })
 })
