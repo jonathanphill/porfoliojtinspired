03d5aecd7bd519cf259681c66013edd014130bc3
jonathanphill <jonathanphill@gmail.com>
2023-05-07 20:19:20 -0400

    added some hover effects on social links, updated some href's for project images

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('it works!');
 const featureItems = document.querySelectorAll('.featureItem');
 const sections = document.querySelectorAll('section');
 const spans = document.getElementsByTagName('span');
-console.log(spans);
+const inspireS = document.querySelectorAll(".inspire");
 
 featureItems.forEach((link)=>{
     link.addEventListener('click', (event)=>{
@@ -31,3 +31,17 @@ const encEmail = "am9uYXRoYW5waGlsbEBnbWFpbC5jb20=";
 const contact = document.getElementById("contact");
 contact.setAttribute("href", "mailto:".concat(atob(encEmail)));
 
+
+inspireS.forEach((anchor)=>{
+    let text = anchor.firstChild.textContent;
+    anchor.addEventListener('mouseover', (event)=>{
+        const text = event.currentTarget.firstElementChild.innerText;
+        event.currentTarget.firstElementChild.innerText= "checkout their awesome work";
+        
+        
+    })
+    anchor.addEventListener("mouseleave", (event) => {
+      event.currentTarget.firstElementChild.innerText =text;
+    });
+    
+})
